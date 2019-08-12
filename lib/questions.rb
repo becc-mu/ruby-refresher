364f8de2837990449041b650cac87bbf8d40bd7c
becc-mu <becc.mulugeta@gmail.com>
2019-08-12 21:42:16 +0100

    total of array

--- lib/questions.rb
+++ lib/questions.rb
@@ -83,15 +83,18 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+  array.min_by(&:length)
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.max_by(&:length)
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+  array.inject(:+)
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
